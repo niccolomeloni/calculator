c9691a330db87d8d40c1fad99f51c46498093580
Niccolò Meloni <niccolomeloni@gmail.com>
2018-02-12 11:07:42 +0100

    START

---
+++ README.md
@@ -0,0 +1,5 @@
+# Calculator
+This laravel demo package will do some calculations
+
+# References
+ * [How to create a Laravel Package](https://devdojo.com/blog/tutorials/how-to-create-a-laravel-package)
---
+++ src/CalculatorServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Niccolomeloni\Calculator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__ . '/routes.php';
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->make('Niccolomeloni\Calculator\CalculatorController');
+        $this->loadViewsFrom(__DIR__ . '/views', 'calculator');
+    }
+}
